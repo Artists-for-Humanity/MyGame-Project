19dbd5453be3f4846ce0e90fe107f743f512f32d
Ctech 21J <ekworthen12@gmail.com>
2023-03-16 17:53:22 -0400

    FINALLY

--- src/scripts/Scenes/MyGameScene.js
+++ src/scripts/Scenes/MyGameScene.js
@@ -1,3 +1,5 @@
+// import { LEFT } from "phaser";
+
 export default class MicroGame31 extends Phaser.Scene {
     // Game Class Constructor
     constructor() {
@@ -10,13 +12,17 @@ export default class MicroGame31 extends Phaser.Scene {
         // Game Object Declarations
         this.myText;
         this.pick_player;
+        this.noteone;
         this.cursors;
-        this.pos = 0;
+        this.pos = ["ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX"]
+        this.posIndex = 0;
+        
     }
 
     preload() {
         this.load.image('frets', new URL('../assets/frets.png', import.meta.url).href);
         this.load.image('pick_player', new URL('../assets/pick_player.png', import.meta.url).href);
+        this.load.image('noteone', new URL('../assets/noteone.png', import.meta.url).href);
     }
 
     create() {
@@ -26,36 +32,58 @@ export default class MicroGame31 extends Phaser.Scene {
         //player
         this.pick_player = this.physics.add.sprite(393,670,'pick_player');
         this.pick_player.setCollideWorldBounds(true);
+        //notes
+        this.noteone = this.physics.add.sprite(393,25, 'noteone');
+        this.noteone.setCollideWorldBounds(true);
+        this.noteone.setVelocityY(500)
         //keyboard
         this.cursors = this.input.keyboard.createCursorKeys();
+        //colliding
+        this.physics.add.collider(this.pick_player, this.noteone, this.onPlayerHitNote, null, this);
+
     }
 
     update() {
-        if (this.cursors.left.isDown) {
-            // this.pos --;
-            this.pick_player.x = 393;
-            //console.log(this.pick_player.x)
-            console.log(this.pos)
-            this.pos = 0;
-            this.setPosition(0);
-        }
-        if (this.cursors.right.isDown) {
-            this.pick_player.x = 451;
-            console.log(this.pos);
-            this.pos = 1; 
-            this.setPosition(1);
-            
-        }
-        
 
+         if (Phaser.Input.Keyboard.JustDown(this.cursors.left)) {
+            this.posIndex--;
+
+        }
+        if (Phaser.Input.Keyboard.JustDown(this.cursors.right)) {
+            this.posIndex++; 
+
+        }
+        if (this.posIndex < 0) {this.posIndex = 0;}
+        else if (this.posIndex > 5) {this.posIndex = 5;}
+
+        if (this.pos[this.posIndex] === "ONE"){
+            this.pick_player.x = 393;
+        }else if(this.pos[this.posIndex] === "TWO"){
+            this.pick_player.x = 451;
+        }else if(this.pos[this.posIndex] === "THREE"){
+            this.pick_player.x = 510;
+        }else if(this.pos[this.posIndex] === "FOUR"){
+            this.pick_player.x = 568;
+        }else if(this.pos[this.posIndex] === "FIVE"){
+            this.pick_player.x = 627;
+        }else if(this.pos[this.posIndex] === "SIX"){
+            this.pick_player.x = 685;
+        }
+
+        if(this.noteone.y === 695){
+            this.resetNote();
+        }
     }
 
-    setPosition(pos){
-        while (pos >= 0 && pos <=5) {
-            break
-            
-        }
+    resetNote(){
+        this.noteone.y = 25
+        this.noteone.setVelocityY(500);
+    }
 
+    onPlayerHitNote(player) {
+        //player.setTint(0xff0000);
+        console.log("player hit")
+        this.resetNote();
     }
 
     setText() {

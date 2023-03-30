2b992c2525229877b6db3210d1475d03e3d8b8cc
Ctech 21J <124737344+eka-kw@users.noreply.github.com>
2023-03-30 17:55:19 -0400

    removed overlap problems from respawnNotes()
    createNotes() is my nemesis >:(((((

--- src/scripts/Scenes/MyGameScene.js
+++ src/scripts/Scenes/MyGameScene.js
@@ -13,9 +13,10 @@ export default class MicroGame31 extends Phaser.Scene {
         //sprites 
         this.pick_player;
         this.posVal = [393, 451, 510, 568, 627, 685]
+        this.pickPosVal = [393, 451, 510, 568, 627, 685]
         this.pickIndex = 0;
         this.notePos = 0;
-        this.numNotes = 2;
+        this.numNotes = 5;
         this.noteSpeed = 500;
         this.notes;
 
@@ -40,7 +41,7 @@ export default class MicroGame31 extends Phaser.Scene {
         //keyboard
         this.cursors = this.input.keyboard.createCursorKeys();
         //colliding
-        this.physics.add.collider(this.pick_player, this.notes, this.onPlayerHitNote, null, this);
+        // this.physics.add.collider(this.pick_player, this.notes, this.onPlayerHitNote, null, this);
         //notes
         this.notes = this.physics.add.group({collideWorldBounds: true});
         this.createNotes();
@@ -59,20 +60,58 @@ export default class MicroGame31 extends Phaser.Scene {
         if (this.pickIndex < 0) {this.pickIndex = 0;}
         else if (this.pickIndex > 5) {this.pickIndex = 5;}
 
-        this.pick_player.x = this.posVal[this.pickIndex];
+        this.pick_player.x = this.pickPosVal[this.pickIndex];
 
-        //colliding **FIX THIS**
+        //colliding 
         this.notes.children.iterate((notes) => {
-            const body = notes.body;
-            if (notes.body.y === 620){
-                this.respawnNote()
+            // const body = notes.body;
+            // console.log(notes.body.y);
+
+            if (notes.body.y === 670){
+                this.respawnNote();
             };
         });
     }
 
     respawnNote(){
+        console.log('reachme 02');
+
+        this.posVal = [393, 451, 510, 568, 627, 685]
         this.notes.setY(25);
-        this.notes.x = this.posVal[(Phaser.Math.Between(0,5))]
+        this.notes.children.iterate((notes) => {
+            const body = notes.body;
+            
+            
+
+            
+            if(this.posVal.length === 6){
+                let temp = (Phaser.Math.Between(0,5));
+                notes.body.x = this.posVal[temp] - 10;
+                this.posVal.splice(temp, 1);
+            }
+            else if (this.posVal.length === 5){
+                let temp = (Phaser.Math.Between(0,4));
+                notes.body.x = this.posVal[temp] - 10;
+                this.posVal.splice(temp, 1);
+            }
+            else if (this.posVal.length === 4){
+                let temp = (Phaser.Math.Between(0,3));
+                notes.body.x = this.posVal[temp] - 10;
+                this.posVal.splice(temp, 1);
+            }
+            else if (this.posVal.length === 3){
+                let temp = (Phaser.Math.Between(0,2));
+                notes.body.x = this.posVal[temp] - 10;
+                this.posVal.splice(temp, 1);
+            }
+            else if (this.posVal.length === 2){
+                let temp = (Phaser.Math.Between(0,1));
+                notes.body.x = this.posVal[temp] - 10;
+                this.posVal.splice(temp, 1);
+            }
+
+
+        });
         this.notes.setVelocityY(this.noteSpeed);
     }
 
@@ -80,6 +119,18 @@ export default class MicroGame31 extends Phaser.Scene {
         for(let i = 0; i < this.numNotes; i++){
             this.notes.create(this.posVal[(Phaser.Math.Between(0,5))],25, 'noteone')
         };
+
+
+        console.log('reachme 00');
+
+        this.respawnNote();
+        this.notes.children.iterate((notes) => {
+            console.log(notes.x)
+        });
+
+        console.log('reachme 01');
+
+
         //this.notes.setCollideWorldBounds(true);
         this.notes.setVelocityY(this.noteSpeed)
         this.physics.add.collider(this.pick_player, this.notes, this.onPlayerHitNote, null, this);
@@ -87,8 +138,9 @@ export default class MicroGame31 extends Phaser.Scene {
 
     onPlayerHitNote(player) {
         //player.setTint(0xff0000);
-        console.log("player hit 0000")
+        // console.log("player hit 0000")
         this.respawnNote();
+        // this.physics.pause();
     }
 
     /* setText() {
